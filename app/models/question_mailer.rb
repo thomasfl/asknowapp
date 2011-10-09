537831bd55539ca66be159c372ff465067bfa090
Thomas Flemming <thomas.flemming@gmail.com>
2011-10-09 19:22:59 +0200

    fixing mailer

--- app/models/question_mailer.rb
+++ app/models/question_mailer.rb
@@ -1,12 +1,26 @@
 # -*- coding: utf-8 -*-
 class QuestionMailer < ActionMailer::Base
 
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com'
+  }
+
+  ActionMailer::Base.raise_delivery_errors = false
+  ActionMailer::Base.perform_deliveries = true
+  ActionMailer::Base.delivery_method = :smtp
+  # ActionMailer::Base.default_content_type = "text/html"
+
   def deliver_html_email(options)
     recipients    options[:recipient]
     subject       options[:subject]
     sent_on       Time.now
     body          options[:body]
-    headers["Content-Type"] = "text/html; charset=UTF-8"
+    # headers["Content-Type"] = "text/html; charset=UTF-8"
   end
 
 end
