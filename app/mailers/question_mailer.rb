8d6e518f4b4751e06cf34203bb7d68d0ab970699
Thomas Flemming <thomas.flemming@gmail.com>
2011-10-12 22:13:29 +0200

    testing mailer

--- app/mailers/question_mailer.rb
+++ app/mailers/question_mailer.rb
@@ -1,6 +1,17 @@
 class QuestionMailer < ActionMailer::Base
   default from: "from@example.com"
 
+
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com'
+}
+ActionMailer::Base.delivery_method = :smtp
+
   def ask
     mail(:to => 'thomas.flemming@gmail.com', :subject => "Welcome to My Awesome Site")
   end
--- config/environment.rb
+++ config/environment.rb
@@ -2,16 +2,6 @@
 require File.expand_path('../application', __FILE__)
 
 
-ActionMailer::Base.smtp_settings = {
-  :address        => 'smtp.sendgrid.net',
-  :port           => '587',
-  :authentication => :plain,
-  :user_name      => ENV['SENDGRID_USERNAME'],
-  :password       => ENV['SENDGRID_PASSWORD'],
-  :domain         => 'heroku.com'
-}
-ActionMailer::Base.delivery_method = :smtp
-
 # Initialize the rails application
 Asknow::Application.initialize!
 
